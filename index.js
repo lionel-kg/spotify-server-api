14429d44c9fabc1960a4c424ad555851f79310bc
lionel-kg <lionel77350@gmail.com>
2023-11-16 13:03:56 +0100

    dynamic convert

--- index.js
+++ index.js
@@ -1,31 +1,54 @@
 const ffmpeg = require('fluent-ffmpeg');
 const fs = require('fs');
+const path = require('path');
 
-function convertMp4ToWav(inputPath, outputPath) {
-  // Vérifier si le fichier d'entrée existe
-  if (!fs.existsSync(inputPath)) {
-    console.error("Le fichier d'entrée n'existe pas.");
+function convertMp4ToWav(songFolder) {
+  // Vérifier si le répertoire d'entrée existe
+  if (!fs.existsSync(songFolder)) {
+    console.error("Le répertoire n'existe pas.");
     return;
   }
 
-  // Utiliser fluent-ffmpeg pour effectuer la conversion
-  ffmpeg(inputPath)
-    .audioCodec('pcm_s16le')
-    .audioChannels(1)
-    .audioFrequency(16000)
-    .format('wav')
-    .on('end', () => {
-      console.log('Conversion terminée avec succès.');
-    })
-    .on('error', err => {
-      console.error('Erreur lors de la conversion :', err);
-    })
-    .save(outputPath);
+  // Répertoire de sortie pour les fichiers WAV
+  const outputFolder = './songs/wav';
+
+  // Assurez-vous que le répertoire de sortie existe, sinon, créez-le
+  if (!fs.existsSync(outputFolder)) {
+    fs.mkdirSync(outputFolder);
+  }
+
+  fs.readdirSync(songFolder, {withFileTypes: true}).forEach(file => {
+    const fileName = file.name;
+
+    // Vérifier si le fichier WAV existe déjà
+    const wavFileName = fileName.replace('.mp4', '.wav');
+    const outputPath = path.join(outputFolder, wavFileName);
+
+    if (fs.existsSync(outputPath)) {
+      console.log(
+        `Le fichier WAV existe déjà pour ${fileName}. La conversion est ignorée.`,
+      );
+      return;
+    }
+
+    // Utiliser fluent-ffmpeg pour effectuer la conversion
+    ffmpeg(path.join(songFolder, fileName))
+      .audioCodec('pcm_s16le')
+      .audioChannels(1)
+      .audioFrequency(16000)
+      .format('wav')
+      .on('end', () => {
+        console.log('Conversion terminée avec succès.');
+      })
+      .on('error', err => {
+        console.error('Erreur lors de la conversion :', err);
+      })
+      .save(outputPath);
+  });
 }
 
-// Spécifiez le chemin du fichier d'entrée (MP4) et de sortie (WAV)
-const inputFilePath = './songs/mp4/Lean Wit Me.mp4';
-const outputFilePath = './songs/wav/test.wav';
+// Spécifiez le chemin du répertoire d'entrée (MP4)
+const songFolder = './songs/mp4';
 
 // Appel de la fonction de conversion
-convertMp4ToWav(inputFilePath, outputFilePath);
+convertMp4ToWav(songFolder);
