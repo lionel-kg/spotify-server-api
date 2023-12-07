88fa30d5a6a1166c9c582e5641deca990572b287
lionel-kg <lionel77350@gmail.com>
2023-12-07 16:31:45 +0100

    comment redis

--- src/middleware/redis.js
+++ src/middleware/redis.js
@@ -2,17 +2,18 @@ import Redis from 'ioredis';
 const redis = new Redis({enableAutoPipelining: true});
 
 const redisMiddleware = async (req, res, next) => {
-  try {
-    const cachedData = await redis.get(req.originalUrl);
-    if (cachedData) {
-      res.status(200).json(JSON.parse(cachedData));
-    } else {
-      next();
-    }
-  } catch (error) {
-    console.error('Cache error:', error);
-    next();
-  }
+  // try {
+  //   const cachedData = await redis.get(req.originalUrl);
+  //   if (cachedData) {
+  //     res.status(200).json(JSON.parse(cachedData));
+  //   } else {
+  //     next();
+  //   }
+  // } catch (error) {
+  //   console.error('Cache error:', error);
+  //   next();
+  // }
+  next();
 };
 
 export default redisMiddleware;
